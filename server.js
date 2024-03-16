4234b8b222c5b6bdb8042275552f39bd1e9d1ef8
rokmc1256 <skh77017@naver.com>
2024-03-16 14:30:06 +0900

    리액트 페이지 띄우기 테스트

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -6,6 +7,9 @@ app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행 중');
 })
 
+app.use(express.static(path.join(__dirname, 'hoseo_graduation/build')));
+
+
 app.get('/', (req, res) => {
-    res.send('반갑다');
+    res.sendFile(path.join(__dirname, 'hoseo_graduation/build/index.html'))
 })
