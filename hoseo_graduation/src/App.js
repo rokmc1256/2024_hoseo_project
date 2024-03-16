e23e3dc82b3e0f2e06476964f3fa68817977736c
rokmc1256 <skh77017@naver.com>
2024-03-16 23:33:33 +0900

    nav바 만듬

--- hoseo_graduation/src/App.js
+++ hoseo_graduation/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as MyLogo } from "./EasyLogo2.svg";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div>리액트로 만든 웹 페이지임</div>
       <div>환영합니다</div>
       <div>리액트 페이지 수정</div>
@@ -14,6 +15,31 @@ function App() {
       <div>윤한2</div>
     </div>
   );
+}
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='navbar_inside navbar_inside1'>
+          <div><MyLogo /></div>
+          <div className='homepagetitle'>
+            <span>E A S Y</span>
+          </div>
+        </div>
+        <div className='navbar_inside navbar_inside2'>
+          <div>
+            <span>마이페이지</span>
+          </div>
+          <div>
+            <span>로그인</span>
+          </div>
+          <div className='inputspace'>
+            <input type ='text' placeholder='검색'></input>
+            <span class="material-symbols-outlined">search</span>
+          </div>
+        </div>
+    </div>
+  )
 }
 
 export default App;
