ded3ab81277b695e34e458fef353462af84de379
rokmc1256 <skh77017@naver.com>
2024-03-24 03:34:00 +0900

    메인페이지와 마이페이지에서 새로고침시 상태초기화 되는 문제 수정

--- hoseo_graduation/src/App.js
+++ hoseo_graduation/src/App.js
@@ -5,15 +5,32 @@ import RegisterForm from './registerForm.js';
 import LoginForm from './loginForm.js';
 import axios from 'axios';
 import MainPage from './mainPage.js';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [usernameInMypage, setUsernameInMypage] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const mainCheckLoggedIn = async () => {
+    try {
+      const response = await axios.get('/checkLoggedIn');
+      const data = response.data;
+      if (data) {
+        setIsLoggedIn(true);
+        setUsernameInMypage(data.username); 
+      }
+    } catch (error) {
+      console.error('오류:', error);
+    }
+  };
+
+  useEffect(() => {
+    mainCheckLoggedIn();
+  }, []);
+
   const checkLoggedIn = async (navigate) => {
     try {
-      const response = await axios.get('/mypage');
+      const response = await axios.get('/checkLoggedIn');
       const data = response.data;
       if (!data) {
         navigate('/login');
@@ -27,67 +44,72 @@ function App() {
   };
 
   const handleLogout = async () => {
-    try{
+    try {
       await axios.get('/logout');
       setIsLoggedIn(false);
-      window.location.href ='/';
-    }catch(err){
-      alert('로그아웃 에러: ', err)
+      setUsernameInMypage(''); 
+      window.location.href = '/';
+    } catch (err) {
+      alert('로그아웃 에러: ', err);
     }
   };
 
   return (
     <div className="App">
-      <Navbar checkLoggedIn = {checkLoggedIn} isLoggedIn ={ isLoggedIn } handleLogout= { handleLogout }/>
+      <Navbar
+        checkLoggedIn={checkLoggedIn}
+        isLoggedIn={isLoggedIn}
+        handleLogout={handleLogout}
+      />
       <Routes>
-        <Route path='/' element={<MainPage />}/>
-        <Route path='/login' element={<LoginForm onLogin={(loggedIn) => setIsLoggedIn(loggedIn)}/>}/>
-        <Route path='/register' element={<RegisterForm />}/>
-        <Route path='/mypage' element={<Mypage usernameInMypage = { usernameInMypage }/>}/>
-      </Routes>  
+        <Route path='/' element={<MainPage />} />
+        <Route path='/login' element={<LoginForm onLogin={(loggedIn) => setIsLoggedIn(loggedIn)} />} />
+        <Route path='/register' element={<RegisterForm />} />
+        <Route path='/mypage' element={<Mypage usernameInMypage={usernameInMypage} />} />
+      </Routes>
     </div>
   );
 }
 
-function Navbar({checkLoggedIn, isLoggedIn, handleLogout}) {
+function Navbar({ checkLoggedIn, isLoggedIn, handleLogout }) {
   const navigate = useNavigate();
   return (
     <div className='navbar'>
-        <div onClick={() => navigate('/')} className='navbar_inside navbar_inside1'>
-          <div><MyLogo /></div>
-          <div className='homepagetitle'>
-            <span>E A S Y</span>
-          </div>
+      <div onClick={() => navigate('/')} className='navbar_inside navbar_inside1'>
+        <div><MyLogo /></div>
+        <div className='homepagetitle'>
+          <span>E A S Y</span>
         </div>
-        <div className='navbar_inside navbar_inside2'>
-          <div onClick={() => checkLoggedIn(navigate)} className='mypage'>
-            <span>마이페이지</span>
-          </div>
-          {isLoggedIn ? (
-          <div onClick={handleLogout} className='logout'> 
+      </div>
+      <div className='navbar_inside navbar_inside2'>
+        <div onClick={() => checkLoggedIn(navigate)} className='mypage'>
+          <span>마이페이지</span>
+        </div>
+        {isLoggedIn ? (
+          <div onClick={handleLogout} className='logout'>
             <span>로그아웃</span>
           </div>
-          ) : (
-          <div onClick={() => navigate('/login')} className='login'> 
-            <span>로그인</span>
-          </div>
+        ) : (
+            <div onClick={() => navigate('/login')} className='login'>
+              <span>로그인</span>
+            </div>
           )}
-          <div onClick={() => navigate('/register')} className='register'>
-            <span>회원가입</span>
-          </div>
-          <div className='inputspace'>
-            <input type ='text' placeholder='검색' />
-            <span class="material-symbols-outlined">search</span>
-          </div>
+        <div onClick={() => navigate('/register')} className='register'>
+          <span>회원가입</span>
         </div>
+        <div className='inputspace'>
+          <input type='text' placeholder='검색' />
+          <span class="material-symbols-outlined">search</span>
+        </div>
+      </div>
     </div>
   )
 }
 
-function Mypage({usernameInMypage}) {
+function Mypage({ usernameInMypage }) {
   return (
-    <div>{usernameInMypage} 님 안녕하세요.</div>
-  ) 
+    <div>{usernameInMypage ? `${usernameInMypage} 님 안녕하세요.` : '로딩 중...'}</div>
+  )
 }
 
 export default App;
