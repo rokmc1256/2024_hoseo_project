dcf562777ded71c13ca684af95ab88894a3c0373
rokmc1256 <skh77017@naver.com>
2024-03-25 02:21:57 +0900

    로컬스토리지를 활용한 글자 깜빡임 버그 수정

--- hoseo_graduation/src/App.js
+++ hoseo_graduation/src/App.js
@@ -5,12 +5,15 @@ import RegisterForm from './registerForm.js';
 import LoginForm from './loginForm.js';
 import axios from 'axios';
 import MainPage from './mainPage.js';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 
 function App() {
-  const [usernameInMypage, setUsernameInMypage] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+  const navigate = useNavigate();
+
+  // 페이지가 새로고침되어도 상태가 유지되도록 localStorage에서 데이터 가져오기
+  const [usernameInMypage, setUsernameInMypage] = useState(localStorage.getItem('usernameInMypage') || '');
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
+
   useEffect(() => {
     const mainCheckLoggedIn = async () => {
       try {
@@ -18,7 +21,10 @@ function App() {
         const { loggedIn, username } = response.data;
         if (loggedIn) {
           setIsLoggedIn(true);
-          setUsernameInMypage(username); 
+          setUsernameInMypage(username);
+          // 로그인 상태와 사용자 이름을 localStorage에 저장
+          localStorage.setItem('isLoggedIn', true);
+          localStorage.setItem('username', usernameInMypage);
         }
       } catch (error) {
         console.error('오류:', error);
@@ -37,6 +43,9 @@ function App() {
       } else {
         setUsernameInMypage(username);
         navigate('/mypage');
+        // 로그인 상태와 사용자 이름을 localStorage에 저장
+        localStorage.setItem('isLoggedIn', true);
+        localStorage.setItem('usernameInMypage', username);
       }
     } catch (error) {
       console.error('오류:', error);
@@ -47,8 +56,11 @@ function App() {
     try {
       await axios.get('/logout');
       setIsLoggedIn(false);
-      setUsernameInMypage(''); 
+      setUsernameInMypage('');
       navigate('/');
+      // 로그아웃 시 localStorage의 데이터 삭제
+      localStorage.removeItem('isLoggedIn');
+      localStorage.removeItem('usernameInMypage');
     } catch (err) {
       alert('로그아웃 에러: ', err);
     }
