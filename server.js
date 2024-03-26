c658cce475b825e5964a714455fd29557183a91c
minseok <149202694+helloimda@users.noreply.github.com>
2024-03-26 20:35:02 +0900

    이미지 게시글 추가

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 const bcrpyt = require('bcrypt')
 const methodOverride = require('method-override')
-
+require('dotenv').config()
 
 app.use(methodOverride('_method')) 
 app.use(express.static(__dirname+'/public'))
@@ -19,13 +19,13 @@ const MongoStore = require('connect-mongo')
 
 app.use(passport.initialize())
 app.use(session({
-  secret: 'hello',
+  secret: process.env.secret,
   resave : false,
   saveUninitialized : false,
   cookie : {maxAge : 60 * 60 * 1000},
 
   store : MongoStore.create({
-    mongoUrl : 'mongodb+srv://admin:mongo123@cluster0.o43budv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    mongoUrl : process.env.DB_URL,
     dbName : 'forum'
     })
 
@@ -40,18 +40,66 @@ const url = 'mongodb+srv://admin:mongo123@cluster0.o43budv.mongodb.net/?retryWri
 new MongoClient(url).connect().then((client)=>{
   console.log('DB연결성공')
   db = client.db('forum')
-  app.listen(8080, function(){
+  app.listen(process.env.PORT, function(){
     console.log("listen 8080")
 });
 }).catch((err)=>{
   console.log(err)
 })
 
+
+const { S3Client } = require('@aws-sdk/client-s3')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const s3 = new S3Client({
+  region : 'ap-northeast-2',
+  credentials : {
+      accessKeyId : process.env.accessKeyId,
+      secretAccessKey : process.env.secretAccessKey
+  }
+})
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'hellogradu',
+    key: function (요청, file, cb) {
+      cb(null, Date.now().toString()) //업로드시 파일명 변경가능
+    }
+  })
+})
+
+
+
+function idpwcheck (요청, 응답, next) { //아이디 비번 null 검사
+  if (요청.body.username == '' || 요청.body.password == '') {
+    응답.send('아이디 혹은 비밀번호를 입력해주세요')
+  } else {
+    next()
+  }
+}
+
+function checklogin(요청, 응답,next){ //로그인 하지 않았다면 로그인알림이 뜨게함
+  
+  if(!요청.user){
+    응답.send('로그인하세요')  
+  }
+  next()
+}
+
+app.use('/URL',checklogin) //모든 API에 체크로그인함수 일괄 적용
+
+app.use('/list', (요청, 응답, next) => { //list시작하는 API는 현재시간 출력
+  console.log(new Date())
+  next()
+}) 
+
 app.get('/pet', function(요청, 응답){
     응답.send(__dirname);
 });
 
 app.get('/',function(요청,응답){
+  
     응답.sendfile(__dirname + '/index.html');
 });
 
@@ -83,14 +131,16 @@ app.get('/write', (요청, 응답) =>{
     응답.render('write.ejs')
 });
 
-app.post('/add', async (요청, 응답) =>{
+app.post('/add',upload.single('img1'), async (요청, 응답) =>{//array 2는 최대 이미지 갯수를 의미함
     let result = 요청.body
     console.log(result)
+    console.log(요청.file.location)
+    console.log('이미지 추가')
     try{
         if (result.title == ''){
             응답.send('제목입력안함 ㅅㄱ')
         }else{
-            await db.collection('post').insertOne({title : 요청.body.title,content:요청.body.content})
+            await db.collection('post').insertOne({title : 요청.body.title,content:요청.body.content, img : 요청.file.location})
             응답.redirect('/list')
         }
     }
@@ -182,7 +232,7 @@ app.get('/edit/:id', async (요청, 응답) => {
     응답.render('register.ejs')
   })
 
-app.post('/register', async (요청, 응답)=> {
+app.post('/register',idpwcheck, async (요청, 응답)=> { //미들웨어함수 아이디비번 null검사
 
     let 해시 = await bcrpyt.hash(요청.body.password, 10)
     console.log(해시)
